a72dae9393f59ff8af39f0e69b472e0bddb85a52
Cleonart <zredhard@gmail.com>
2021-10-06 21:37:46 +0800

    add support speech recognition

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,18 @@
 const HOST = "wss://dlsu-hearme.herokuapp.com";
 
 let app = new Vue({
-    el : "#app",
-    data : {
-        soundGauge : null,
-        soundValue : 0,
-        connection : null,
-        iotDeviceStatus : 0
+    el: "#app",
+    data: {
+        soundGauge: null,
+        soundValue: 0,
+        connection: null,
+        iotDeviceStatus: 0,
+        transcriptData: "",
+        action: ""
     },
-    methods : {
+    methods: {
 
-        initalizeSoundGauge : function(){
+        initalizeSoundGauge: function () {
             let opts = {
                 angle: 0.35,                // The span of the gauge arc
                 lineWidth: 0.1,             // The line thickness
@@ -36,39 +38,70 @@ let app = new Vue({
             this.soundGauge.set(925);                               // set actual value
         },
 
-        initializeWebSocket : function(){
+        initializeWebSocket: function () {
             let app = this;
             this.connection = new WebSocket(HOST);
 
             // Set Status to Connecting
             app.iotDeviceStatus = 100;
-            
+
             // If Successfully Connected to WSS
-            this.connection.onopen = function(event) {
+            this.connection.onopen = function (event) {
                 app.iotDeviceStatus = 200;
                 app.connection.send("Requesting Connection From Client");
             }
 
             // On Message 
-            this.connection.onmessage = function(event){
+            this.connection.onmessage = function (event) {
                 console.log(event);
                 app.soundValue = parseInt(event.data);
                 app.soundGauge.set(parseInt(event.data));
             }
-            
+
             // If Connection to WSS Closed
-            this.connection.onclose = function(event){
-                app.iotDeviceStatus = 500;
-            }
-            
-            this.connection.onerror = function(event){
+            this.connection.onclose = function (event) {
                 app.iotDeviceStatus = 500;
             }
 
+            this.connection.onerror = function (event) {
+                app.iotDeviceStatus = 500;
+            }
+
+        },
+
+        runSpeechRecognition: function () {
+
+            var app = this;
+
+            // new speech recognition object
+            var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+            var recognition = new SpeechRecognition();
+            recognition.lang = "id-ID"
+
+            // This runs when the speech recognition service starts
+            recognition.onstart = function () {
+                app.action = "Listening...";
+            };
+
+            recognition.onspeechend = function () {
+                app.action = "Done Listening...";
+                recognition.stop();
+            }
+
+            // This runs when the speech recognition service returns result
+            recognition.onresult = function (event) {
+                var transcript = event.results[0][0].transcript;
+                var confidence = event.results[0][0].confidence;
+                app.transcriptData = transcript;
+            };
+
+            // start recognition
+            recognition.start();
         }
+
     },
-    mounted(){
-        this.initalizeSoundGauge();  
+    mounted() {
+        this.initalizeSoundGauge();
         this.initializeWebSocket();
     }
 })
